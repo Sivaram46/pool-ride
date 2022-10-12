af3c1a58bdfea6879e0c27304e7b2f5ea1a761d0
coderman.hx <coderman.hx@gmail.com>
2022-10-13 00:48:27 +0530

    added sessions

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
+from polyline import Polyline
 
+counter = 0
 app = Flask(__name__, template_folder="template")
+polyline = Polyline()
+
+app.secret_key = b's/,mdnfklsadn'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    result = [None, None]
+    global counter
+    if request.method == 'GET':
+        counter += 1
+        session['id'] = counter
+        return render_template('index.html')
+
     if request.method == 'POST':
         source = (request.form.get("source-lon"), request.form.get("source-lat"))
         dest = (request.form.get("dest-lon"), request.form.get("dest-lat"))
-        result = [source, dest]
+        polyline.add(session['id'], source, dest)
 
-    return render_template('index.html', data=result)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
